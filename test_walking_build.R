63e00307635034bfe5a37686cd93cb085405df26
phildwalker <philwalker2012@gmail.com>
2020-11-04 14:12:44 -0500

    added to walking iso

--- test_walking_build.R
+++ test_walking_build.R
@@ -20,7 +20,30 @@ options(osrm.profile = "walk")
 
 iso <- osrmIsochrone(loc = c(-79.77018000,36.06832000),
                      returnclass="sf",
-                     breaks = c(2,5,10,30),
-                     res = 60)
+                     breaks = c(1,2,4,5,9,10, 20),
+                     res = 50)
+
+
+# st_write(iso_location, here::here("data", "walk_richardson","isochrones_walk_richardson.shp"), delete_dsn = T)
+
+library(leaflet)
+steps <- sort(as.numeric(iso$max))
+
+iso <- cbind(steps = steps[iso[['id']]], iso)
+pal <- colorFactor(viridis::plasma(nrow(iso), direction = -1), 
+                   iso$steps)
+
+leaflet() %>% 
+  addTiles() %>% 
+  addPolygons(data = iso,
+              weight = 2, 
+              color = ~pal(steps)) %>% 
+  addLegend(data = iso,
+            pal = pal, 
+            values = ~steps,
+            title = 'Drive Time (min.)',
+            opacity = 1) %>%
+  addMarkers(loc$long, loc$lat) %>%
+  setView(loc$long, loc$lat, zoom = 13)
 
 
